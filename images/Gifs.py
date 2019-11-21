9750a1757c8acc421024858d8ccf92c64ac9b8c0
Ngina Maingi <ngina_maingi@yahoo.com>
2019-11-21 10:28:22 -0600

    images folder added

---
+++ images/Gifs.py
@@ -0,0 +1,6 @@
+
+1= <iframe src="https://giphy.com/embed/2erqKj7wV12bdoPagM" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/nest-bird-crash-2erqKj7wV12bdoPagM">via GIPHY</a></p>
+
+2= <iframe src="https://giphy.com/embed/68yBjfxSpUpUY" width="480" height="269" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/tom-and-jerry-flattened-68yBjfxSpUpUY">via GIPHY</a></p>
+
+3= <iframe src="https://giphy.com/embed/oe33xf3B50fsc" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/explosion-oe33xf3B50fsc">via GIPHY</a></p>
